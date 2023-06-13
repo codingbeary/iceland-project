3ffca11849f1046b660bcaec10bf00931564f71c
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-06-13 13:57:06 +0100

    centralised vertical alignment of header links

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const scrollButton = document.getElementById("scrollDown");
 const aboutSection = document.getElementById("about");
+const navLinks = document.querySelectorAll("");
 
 scrollButton.addEventListener("click", () => {
   aboutSection.scrollIntoView({ behavior: "smooth" });
